de430cfde2f9ce872d3c8d62212ca08ed8772fdb
acbaez9 <97056049+acbaez9@users.noreply.github.com>
2024-08-21 17:33:00 -0700

    fixed hugginface downloader

--- sat/dataset/hf_downloader.py
+++ sat/dataset/hf_downloader.py
@@ -1,9 +1,9 @@
 import os
+import argparse
 from huggingface_hub import HfApi, hf_hub_download
 from tqdm import tqdm
 
-def download_dataset(repo_id, token=None, download_dir="./downloaded_videos"):
-    
+def download_dataset(repo_id, token=None, download_dir="./videos", num_files=None):
     # Initialize Hugging Face API client
     api = HfApi(token=token)
     
@@ -13,6 +13,9 @@ def download_dataset(repo_id, token=None, download_dir="./downloaded_videos"):
     # List all files in the repository
     files = api.list_repo_files(repo_id)
     
+    # Limit the number of files if max_files is specified
+    if num_files is not None:
+        files = files[:num_files]
     
     # Download each MP4 file
     for file in tqdm(files):
@@ -27,10 +30,14 @@ def download_dataset(repo_id, token=None, download_dir="./downloaded_videos"):
         )
         
         print(f"Downloaded {file}")
-    
 
 if __name__ == "__main__":
-    hf_repo = "DeepAIResearch/test_dataset"
-    hf_token = "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"
+    parser = argparse.ArgumentParser(description="Download dataset from HuggingFace Hub")
+    parser.add_argument("--repo", type=str, default="DeepAIResearch/SpatialSceneSyntheticDataset", help="HuggingFace repository ID")
+    parser.add_argument("--token", type=str, default="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", help="HuggingFace API token")
+    parser.add_argument("--output", type=str, default="./videos", help="Output directory for downloaded files")
+    parser.add_argument("--num_files", type=int, default=None, help="Maximum number of files to download (default: all)")
+    
+    args = parser.parse_args()
 
-    download_dataset(hf_repo, hf_token)
+    download_dataset(args.repo, args.token, args.output, args.num_files)
